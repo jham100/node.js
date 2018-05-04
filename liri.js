ecadd304888ff9dab90ef0ca1bdadf616df8114b
Julian Hamilton <Ham@julians-mbp.attlocal.net>
2018-05-03 17:00:45 -0700

    best i could do

---
+++ liri.js
@@ -0,0 +1,25 @@
+
+require("dotenv").config();
+var request = require("request");
+var requestp = require("request-promise");
+var Spotify = require('node-spotify-api');
+
+var keys = require('./keys.js');
+
+
+var spotify = new Spotify({
+    id:keys.spotify.id,
+    secret:keys.spotify.secret,
+  });
+   
+  spotify.search({ type: 'track', query: process.argv[2]}, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  JSON.stringify(data); 
+  console.log(data);
+  });
+  console.log("Enter a song name to search for it-")
+// I think that I have almost gotten this exercise to work, 
+// but I am having an issue getting the response that I am looking for.
+
